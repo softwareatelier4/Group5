a20da61d9b826c9f0a2075fe3b99e68c9504d66e
Alexander Fischer <alexander.fischer@usi.ch>
2017-03-15 16:26:56 +0100

    Re-added Nightwatch, as well as one extra test.

--- frontend/test/jobadvisor-app/test-search-visualize.js
+++ frontend/test/jobadvisor-app/test-search-visualize.js
@@ -4,6 +4,7 @@ module.exports = {
       .url('http://localhost:8080/')
       .waitForElementVisible('body', 1000)
       .waitForElementVisible('ja-results-list', 1000)
+      .waitForElementVisible('#results-title', 1000)
       .end();
   }
 };
