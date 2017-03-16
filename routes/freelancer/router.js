f9121d6b6f002e40db301e84007867b6dc99852a
francescosani <sanif@usi.ch>
2017-03-16 16:19:25 +0100

    added freelancer router; made search case insensitive

---
+++ routes/freelancer/router.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const middleware =  require('../middleware');
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+const Freelancer = mongoose.model('Freelancer');
+const config = require('../../config');
+
+const fieldsFilter = { '__v': 0 };
+
+router.all('/', middleware.supportedMethods('GET'));
+
+router.get('/:freelancerid', function(req, res, next) {
+  Freelancer.findById(req.params.freelancerid, fieldsFilter).lean().exec(function(err, freelancer){
+    if (err) return next (err);
+    if (!freelancer) {
+      res.status(404);
+      res.json({
+        statusCode: 404,
+        message: "Not Found"
+      });
+      return;
+    }
+    addLinks(freelancer);
+    res.json(freelancer);
+  });
+});
+
+function addLinks(freelancer){
+  freelancer.links = [
+    {
+      "rel" : "self",
+      "href" : config.url + "/freelancer/" + freelancer._id
+    }
+  ];
+}
+
+module.exports = router;
