1bfc87cda2eb4310e8074ce5752abb1a71e4871f
francescosani <sanif@usi.ch>
2017-03-17 16:02:31 +0100

    registered freelancer router, added redirect for unkown routes

--- app.js
+++ app.js
@@ -34,6 +34,18 @@ var routers = require('./routes/routers');
 app.use('/', routers.root);
 
 app.use('/search', routers.search);
+app.use('/freelancer', routers.freelancer);
+
+app.use('*', function(req,res, next){
+  if(req.accepts('html')){
+    const options = {
+        root: __dirname + '/frontend/',
+      };
+    return res.sendFile('index.html', options);
+  }
+
+  next();
+})
 
 module.exports = app;
 process.title = 'JobAdvisor'
