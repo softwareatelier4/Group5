6853a8de223eda5270165cc66d4d3fda3a24425b
Camillo Malnati <malnac@usi.ch>
2017-03-17 15:34:23 +0100

    completed backend test

--- test/seedData.js
+++ test/seedData.js
@@ -4,63 +4,64 @@ var mongoose = require('mongoose');
 var ObjectId = mongoose.Types.ObjectId;
 
 var users = {
- name : 'User',
- data : [
-  {
-    "firstName"   : "",
-   "lastName"     : "",
-   "userName"     : "Bob",
-   "password"     : "Bob",
-   "email"        : "ciaoisnadis",
-   "comm_rating"  : 9
-  },
- ]
+  name : 'User',
+  data : [
+    {
+      "firstName"    : "",
+      "lastName"     : "",
+      "userName"     : "Bob",
+      "password"     : "Bob",
+      "email"        : "ciaoisnadis",
+      "comm_rating"  : 9
+    },
+  ]
 }
 
-var files = {
- name : 'Freelancer',
- data : [
-  {
-   "firstName"          : "Mario",
-   "lastName"         : "Rossi",
-   "address"       : "Via san gottardo 12",
-   "description"   : "Description",
-   "rating" : 3,
-   "email" : "ciao@yahoo.com",
-   "phone_number" : "+41 4442323223",
-   "location" : "Lugano",
-   "price" : 300,
-   "profession": "Painter"
-  },
-  {
-   "firstName"          : "Luca",
-   "lastName"         : "Bernasconi",
-   "address"       : "Via coihsaoidf",
-   "description"   : "Description",
-   "rating" : 3,
-   "email" : "ciao@yahoo.com",
-   "phone_number" : "+41 4442323223",
-   "location" : "Bellinzona",
-   "price" : 300,
-   "profession": "Plumber"
-  },
-  {
-   "firstName"          : "Giovanni",
-   "lastName"         : "Rezzonico",
-   "address"       : "Via coihsaoidf",
-   "description"   : "Description",
-   "rating" : 3,
-   "email" : "ciao@yahoo.com",
-   "phone_number" : "+41 4442323223",
-   "location" : "Lugano",
-   "price" : 300,
-   "profession":"painter"
-  }
- ]
+var freelancer = {
+  name : 'Freelancer',
+  data : [
+    {
+      "_id"           : ObjectId("5625fc2bd82b84d23d8c7bd5"),
+      "firstName"     : "Mario",
+      "lastName"      : "Rossi",
+      "address"       : "Via san gottardo 12",
+      "description"   : "Description",
+      "rating"        : 3,
+      "email"         : "ciao@yahoo.com",
+      "phone_number"  : "+41 4442323223",
+      "location"      : "Lugano",
+      "price"         : 300,
+      "profession"    : "Painter"
+    },
+    {
+      "firstName"     : "Luca",
+      "lastName"      : "Bernasconi",
+      "address"       : "Via coihsaoidf",
+      "description"   : "Description",
+      "rating"        : 3,
+      "email"         : "ciao@yahoo.com",
+      "phone_number"  : "+41 4442323223",
+      "location"      : "Bellinzona",
+      "price"         : 300,
+      "profession"    : "Plumber"
+    },
+    {
+      "firstName"     : "Giovanni",
+      "lastName"      : "Rezzonico",
+      "address"       : "Via coihsaoidf",
+      "description"   : "Description",
+      "rating"        : 3,
+      "email"         : "ciao@yahoo.com",
+      "phone_number"  : "+41 4442323223",
+      "location"      : "Lugano",
+      "price"         : 300,
+      "profession"    :"painter"
+    }
+  ]
 }
 
 var seedData = [];
-seedData.push(files);
+seedData.push(freelancer);
 seedData.push(users);
 
 module.exports = seedData;
