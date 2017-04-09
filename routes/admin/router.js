c35c8fbba60b4993a1374235d49d84a7b550e7f7
samuelebischof <samuele.bischof@usi.ch>
2017-04-09 18:12:01 +0200

    Test button for email

--- routes/admin/router.js
+++ routes/admin/router.js
@@ -4,15 +4,22 @@
 const express = require('express');
 const router = express.Router();
 const middleware =  require('../middleware');
-const rootUrl = require("../../config").url;
+const rootUrl = require('../../config').url;
+const mail = require('../../mail/mail');
+
 
 
 //supported methods
-router.all('/', middleware.supportedMethods('GET, OPTIONS'));
+router.all('/', middleware.supportedMethods('GET, POST, OPTIONS'));
 
 //list users
 router.get('/', function(req, res, next) {
   res.sendFile('admin.html', { root: 'frontend/' });
 });
+
+router.post('/', function(req, res) {
+  mail.sendMailTo(req.query.address, req.query.content, req.query.subject)
+});
+
 /** router for /users */
 module.exports = router;
