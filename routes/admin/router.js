36cbda6dfed2600fc99a6f231bc0612e31798070
Lorenzo Spoleti <spolel@usi.ch>
2017-04-10 15:01:23 +0200

    working on displaying the pending users

--- routes/admin/router.js
+++ routes/admin/router.js
@@ -3,9 +3,13 @@
 
 const express = require('express');
 const router = express.Router();
-const middleware =  require('../middleware');
+const middleware = require('../middleware');
 const rootUrl = require('../../config').url;
 const mail = require('../../mail/mail');
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+const Freelancer = mongoose.model('Freelancer');
+const config = require('../../config');
 
 
 
@@ -13,11 +17,23 @@ const mail = require('../../mail/mail');
 router.all('/', middleware.supportedMethods('GET, POST, OPTIONS'));
 
 //list users
-router.get('/', function(req, res, next) {
-  res.sendFile('admin.html', { root: 'frontend/' });
+router.get('/', function (req, res, next) {
+  res.sendFile('admin.html', {
+    root: 'frontend/'
+  });
 });
 
-router.post('/', function(req, res) {
+router.get('/pending/', function (req, res, next) {
+  res.status(200);
+
+  Freelancer.find().exec(function (err, profiles) {
+    if (err) return console.error(err);
+    res.json(profiles);
+  });
+
+});
+
+router.post('/', function (req, res) {
   mail.sendMailTo(req.query.address, req.query.content, req.query.subject)
 });
 
