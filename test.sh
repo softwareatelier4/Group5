81b5e3d79643e2ce1d9671a943bd6bb24195edce
Alexander Fischer <alexander.fischer@usi.ch>
2017-04-19 13:16:47 +0200

    Changed run commands for jenkins.

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
+npm install
 npm run install-jenkins
 npm run test-mocha
 npm run start-jenkins &
+# ./bin/installSeleniumAndChromeWebDriver
+node seed.js
 xvfb-run -a ./nightwatch
 pkill "JobAdvisor"
