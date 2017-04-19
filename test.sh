c00c9d622081455f1543c17310037ecf3ce04d88
Alexander Fischer <alexander.fischer@usi.ch>
2017-04-19 13:10:38 +0200

    Modified run commands.

--- test.sh
+++ test.sh
@@ -1,11 +1,5 @@
-npm install
 npm run install-jenkins
 npm run test-mocha
 npm run start-jenkins &
-# ./bin/installSeleniumAndChromeWebDriver
-node seed.js
-cd frontend
-bower install
-cd ..
 xvfb-run -a ./nightwatch
 pkill "JobAdvisor"
