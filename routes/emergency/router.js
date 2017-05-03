540ebc7e099239677bd2d3383072cdfc4d52f0b7
Emanuele <esposem@usi.ch>
2017-05-03 15:19:22 +0200

    added something

--- routes/emergency/router.js
+++ routes/emergency/router.js
@@ -18,6 +18,8 @@ router.post('/', function(req, res, next) {
 
   res.status(201);
 
+  let time = req.body.time; //TODO
+
   var newJson = {};
   newJson['profession'] = new RegExp(req.body.profession, 'i');
   newJson['category'] = new RegExp(req.body.category, 'i');
@@ -31,18 +33,33 @@ router.post('/', function(req, res, next) {
 
   Freelancer.find(newJson).exec(function(err, profiles) {
     if (err) return console.error(err);
-
+    // profiles is array of objects {}
     if (profiles === undefined || profiles.length == 0) {
       console.log("No matching profiles were found");
-      //TODO: do something here
     } else {
+      console.log("found " + profiles.length + " freelancers!");
+
       var dest = [];
+
       profiles.forEach(function(profile) {
-        dest.push(profile.address);
+        var rightone = false;
+        for(let j = 0; j < profile.events.length; j++){
+          var d = Date.now();
+          if(profile.events[j].start <= d && d <= profile.events[j].end){
+            rightone = true;
+            dest.push(profile.events[j].location);
+            break;
+          }
+        }
+
+        if(rightone === false){
+          profiles.splice(profiles.indexOf(profile), 1);
+        }
+
       });
+
       var dest_joined = dest.join('|')
 
-      // TODO: user req location to check against profiles
       var googleMapsClient = require('@google/maps').createClient({
         key: 'AIzaSyAolcHbiX1slqHH0Vv3F_YC2fI_0JGFGfQ'
       });
@@ -53,9 +70,19 @@ router.post('/', function(req, res, next) {
         destinations : dest_joined,
       }
 
+      // distances is an array of values
+      var distances = [];
       googleMapsClient.distanceMatrix(distanceQuery, function(err, res) {
         if (err) return console.error(err);
-        console.log(res.json.rows[0].elements[0].distance.text);
+        for(var i = 0; i < profiles.length; i++){
+
+          profiles[i] = profiles[i].toObject();
+          profiles[i].distance = res.json.rows[0].elements[i].distance.value;
+        }
+
+        profiles.sort(function(a,b){
+          return a.distance > b.distance;
+        })
 
       });
 
