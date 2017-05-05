3515558edc8d424a6156292caf18e6c653a357fe
Camillo Malnati <malnac@usi.ch>
2017-05-05 10:53:59 +0200

    working on better server errors handling

--- routes/freelancer/router.js
+++ routes/freelancer/router.js
@@ -11,6 +11,7 @@ const Freelancer = mongoose.model('Freelancer');
 const Review = mongoose.model('Review');
 const CalendarEvent = mongoose.model('CalendarEvent');
 const config = require('../../config');
+const serverErrors = require('../serverErrors')
 
 const fieldsFilter = { '__v': 0 };
 
@@ -19,18 +20,10 @@ router.all('/', middleware.supportedMethods('GET, POST, DELETE'));
 router.get('/:freelancerid', function(req, res, next) {
   Freelancer.findById(req.params.freelancerid, fieldsFilter).lean().exec(function(err, freelancer){
     if (err) {
-      res.status(400)
-      return res.json({
-        statusCode: 400,
-        message: "Bad Request"
-      });
+      return res.status(400).json(serverErrors.badRequest);
     }
     if (!freelancer) {
-      res.status(404);
-      res.json({
-        statusCode: 404,
-        message: "Not Found"
-      });
+      return res.status(404).json(serverErrors.notFound);
       return;
     }
     res.json(freelancer);
@@ -41,27 +34,15 @@ router.post('/:freelancerid/review', function(req, res, next) {
   var toAdd = new Review(req.body);
   toAdd.save(function(err) {
     if (err) {
-      res.status(400)
-      return res.json({
-        statusCode: 400,
-        message: "Bad Request"
-      });
+      return res.status(400).json(serverErrors.badRequest);
     }
     Freelancer.findByIdAndUpdate(req.params.freelancerid, {$push: {"reviews": toAdd}},
     {safe: true, upsert: true, new : false},
     function(err, freelancer) {
       if (err) {
-        res.status(400)
-        return res.json({
-          statusCode: 400,
-          message: "Bad Request"
-        });
+        return res.status(400).json(serverErrors.badRequest);
       }else if (!freelancer) {
-        res.status(400)
-        return res.json({
-          statusCode: 400,
-          message: "Bad Request"
-        });
+        return res.status(400).json(serverErrors.badRequest);
       } else {
         return res.json(toAdd._id);
       }
@@ -73,27 +54,15 @@ router.post('/:freelancerid/event', function(req, res, next) {
   var toAdd = new CalendarEvent(req.body);
   toAdd.save(function(err) {
     if (err) {
-      res.status(400)
-      return res.json({
-        statusCode: 400,
-        message: "Bad Request"
-      });
+      return res.status(400).json(serverErrors.badRequest);
     }
     Freelancer.findByIdAndUpdate(req.params.freelancerid, {$push: {"events": toAdd}},
     {safe: true, upsert: true, new : false},
     function(err, freelancer) {
       if (err) {
-        res.status(400)
-        return res.json({
-          statusCode: 400,
-          message: "Bad Request"
-        });
+        return res.status(400).json(serverErrors.badRequest);
       } else if (!freelancer) {
-        res.status(400)
-        return res.json({
-          statusCode: 400,
-          message: "Bad Request"
-        });
+        return res.status(404).json(serverErrors.notFound);
       } else {
         return res.json(toAdd);
       }
@@ -106,26 +75,14 @@ router.delete('/:freelancerid/event/:eventid', function(req, res, next) {
     if(toDel){
       Freelancer.findByIdAndUpdate(req.params.freelancerid, { $pull: { events : toDel } }, function(err, freelancer){
         if(err){
-          res.status(400)
-          return res.json({
-            statusCode: 400,
-            message: "Bad Request"
-          });
+          return res.status(400).json(serverErrors.badRequest);
         }else if (!freelancer) {
-          res.status(400)
-          return res.json({
-            statusCode: 400,
-            message: "Bad Request"
-          });
+          return res.status(404).json(serverErrors.notFound);
         } else{
           CalendarEvent.remove(toDel,
             function(err) {
               if (err) {
-                res.status(400)
-                return res.json({
-                  statusCode: 400,
-                  message: "Bad Request"
-                });
+                return res.status(400).json(serverErrors.badRequest);
               } else {
                 return res.json({
                   _id : req.params.eventid,
@@ -137,25 +94,22 @@ router.delete('/:freelancerid/event/:eventid', function(req, res, next) {
           }
         })
       }else{
-        res.status(400)
-        return res.json({
-          statusCode: 400,
-          message: "Bad Request"
-        });
-      }});
-    });
-
-    router.post('/:n', function(req, res, next) {
-      let n = req.params.n;
-      var toAdd = new Freelancer(req.body);
-      if (n == 1){
-        toAdd.image = "/src/images/" + toAdd._id + ".png";
-      } else {
-        toAdd.image = "/src/images/blank-user.jpg";
+        return res.status(404).json(serverErrors.notFound);
       }
-      toAdd.save();
-      res.json(toAdd._id);
     });
+  });
+
+  router.post('/:n', function(req, res, next) {
+    let n = req.params.n;
+    var toAdd = new Freelancer(req.body);
+    if (n == 1){
+      toAdd.image = "/src/images/" + toAdd._id + ".png";
+    } else {
+      toAdd.image = "/src/images/blank-user.jpg";
+    }
+    toAdd.save();
+    res.json(toAdd._id);
+  });
 
 
-    module.exports = router;
+  module.exports = router;
---
+++ routes/serverErrors.js
@@ -0,0 +1,10 @@
+module.exports.serverError = {
+  badRequest: {
+    statusCode  : 400,
+    message     : "Bad Request"
+  },
+  notFound:    {
+    statusCode  : 404,
+    message     : "Not Found"
+  },
+}
