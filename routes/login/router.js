17f96c44d0ac8da1fb21d53efba22a87c83d567b
Davide Bucher <shughi@davides-mbp.mobile.usilu.net>
2017-05-06 10:57:18 +0200

    login now return freelancerId too

--- routes/login/router.js
+++ routes/login/router.js
@@ -25,6 +25,7 @@ router.post('/', function(req, res, next) {
             userName: user.userName,
             userType: user.userType,
             _id: user._id,
+            _freelancerId: user.freelancerId,
           }
         })
       }else{
