28199294280bcd641f3191c628b856871d9ff976
spolel <spolel@usi.ch>
2017-05-18 10:11:40 +0200

    fixing test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 npm install
 npm run install-jenkins
 npm run test-mocha
-npm run start-jenkins &
+# npm run start-jenkins &
 # ./bin/installSeleniumAndChromeWebDriver
 # node seed.js
 # xvfb-run -a ./nightwatch
