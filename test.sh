f4e657b38734dda026e96618bf9569377901419e
spolel <spolel@usi.ch>
2017-05-18 10:06:26 +0200

    jenkins has problems

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ npm run install-jenkins
 npm run test-mocha
 npm run start-jenkins &
 # ./bin/installSeleniumAndChromeWebDriver
-node seed.js
+# node seed.js
 # xvfb-run -a ./nightwatch
 pkill "JobAdvisor"
